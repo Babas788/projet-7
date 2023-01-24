4f602d299a2597126cdd57ce04b21debd3149ad3
Babas788 <agi.bastien@hotmail.fr>
2023-01-24 09:59:58 +0100

    valid

---
+++ kasa/src/components/banner/index.jsx
@@ -0,0 +1,10 @@
+function Banner({ image, text }) {
+  return (
+    <div className="banner">
+      <img src={image} className="bannerImg" alt="bannerImg" />
+      <p className="text">{text}</p>
+    </div>
+  );
+}
+
+export default Banner;
