c1eaafb5c1565e846b13aa310486704ce046d963
Babas788 <agi.bastien@hotmail.fr>
2023-02-04 09:28:59 +0100

    dernières modifications

--- kasa/src/components/collapse/index.jsx
+++ kasa/src/components/collapse/index.jsx
@@ -12,9 +12,22 @@ function Collapse ({ title, content }) {
         <div>{isActive ?<img src={CollapseUp} alt="arrowDown" className='arrow_down'/> 
         : <img src={CollapseDown} alt="arrowDown" className='arrow_down'/>}</div>
       </div>
-      {isActive && <div className="collapse_text">{content}</div>}
+      {isActive && 
+      <div className="collapse_text">
+        {Array.isArray(content) ? (
+          <ul className="collapse__list">
+            {content.map((equipment, index) => (
+              <li key={index} className="collapse__list-element">
+                {equipment}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="collapse__text">{content}</p>
+        )}</div>}
     </div>
   );
 };
 
 export default Collapse;
+
